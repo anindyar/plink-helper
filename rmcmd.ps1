b430efab887cb0bb22ebf9391ea5bb6dc63005f5
Anidnya Roy <i@anindya.me>
2020-12-18 06:29:30 +0400

    Create rmcmd.ps1

---
+++ rmcmd.ps1
@@ -0,0 +1,21 @@
+$SudoOrNot = Read-Host -Prompt 'Do you want to run your command in sudo? Y/N'
+$lineNum = get-content .\ip.txt | measure-object -line | select -expand lines
+echo $lineNum
+$file_data = Get-Content .\ip.txt 
+$commands = Get-Content .\commands.txt |% {$_-replace "`r", "";"`n "}
+
+$line = 0
+
+While ($line -lt $lineNum)
+     {
+      if($SudoOrNot -eq 'Y') {
+        plink.exe -no-antispoof -ssh $file_data -l $args[0] -pw $args[1] "echo -e "$args[1]" | sudo -S "$commands""
+    }else {
+        plink.exe -no-antispoof -ssh $file_data -l $args[0] -pw $args[1] $commands
+    }
+
+    $line++
+ } 
+
+
+

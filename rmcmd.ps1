4a3deee7746066fd173cd6208b3c497161b1a852
Anidnya Roy <i@anindya.me>
2020-12-18 06:37:50 +0400

    Update rmcmd.ps1

--- rmcmd.ps1
+++ rmcmd.ps1
@@ -9,9 +9,9 @@ $line = 0
 While ($line -lt $lineNum)
      {
       if($SudoOrNot -eq 'Y') {
-        plink.exe -no-antispoof -ssh $file_data -l $args[0] -pw $args[1] "echo -e "$args[1]" | sudo -S "$commands""
+        plink.exe -no-antispoof -ssh $file_data[$lineNum] -l $args[0] -pw $args[1] "echo -e "$args[1]" | sudo -S "$commands""
     }else {
-        plink.exe -no-antispoof -ssh $file_data -l $args[0] -pw $args[1] $commands
+        plink.exe -no-antispoof -ssh $file_data[$lineNum] -l $args[0] -pw $args[1] $commands
     }
 
     $line++
